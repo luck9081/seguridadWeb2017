2d48ae2d47d54672c04a87bb6f9d35ca6d6cd647
luck9081 <luck_m_r@hotmail.com>
2017-04-30 18:23:51 -0300

    Creación de Controller
    - Eliminación de InvitadoBean

---
+++ src/main/java/ar/edu/unlam/diit/scaw/beans/Controller.java
@@ -0,0 +1,55 @@
+package ar.edu.unlam.diit.scaw.beans;
+
+import java.io.Serializable;
+
+import javax.enterprise.context.RequestScoped;
+import javax.faces.bean.ManagedBean;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+@ManagedBean(name = "controller", eager = true)
+@RequestScoped
+public class Controller implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+		
+	//Spring Inject
+	ApplicationContext context = new ClassPathXmlApplicationContext(new String[] {"beans.xml"});
+	
+	
+	public Controller() {
+		super();
+	}
+	
+	public String usuarioHome(){
+		return "usuario";
+	}
+	
+	public String adminHome(){
+		return "admin";
+	}
+	
+	public String compartir(){
+		return "compartir";
+	}
+	
+	public String privilegios(){
+		return "privilegios";
+	}
+	
+	public String login(){
+		return "usuario";
+	}
+	
+	public void logout(){
+		
+	}
+	
+	public String registro(){
+		return "index";
+	}
+
+
+}
