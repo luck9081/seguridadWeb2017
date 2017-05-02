8a6bc7135715089e5d9cc84c0ff1f3762c5262d4
luck9081 <luck_m_r@hotmail.com>
2017-05-02 13:10:01 -0300

    Modificaciones menores a SessionBean

--- src/main/java/ar/edu/unlam/diit/scaw/beans/SessionBean.java
+++ src/main/java/ar/edu/unlam/diit/scaw/beans/SessionBean.java
@@ -3,26 +3,21 @@ package ar.edu.unlam.diit.scaw.beans;
 import java.io.Serializable;
 
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.SessionScoped;
 
-@ManagedBean(name = "session", eager = true)
+@ManagedBean(name = "miSesion")
 @SessionScoped
 public class SessionBean implements Serializable {
 
-	private static final long serialVersionUID = 1L;
-	
-	
-	@ManagedProperty(value = "#{idUsuario}")
+	private static final long serialVersionUID = 1L;	
 	private String idUsuario;
-
+	
 	public String getIdUsuario() {
 		return idUsuario;
 	}
 
 	public void setIdUsuario(String idUsuario) {
 		this.idUsuario = idUsuario;
-	}
-	
+	}	
 	
 }
